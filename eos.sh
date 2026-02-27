097d7ff2d64f33f592fc869901edf1328994a320
IlyaGredasov <iljagredasov@yandex.ru>
2026-02-27 13:15:26 +0300

    feat: add eos.sh repair

---
+++ eos.sh
@@ -0,0 +1,9 @@
+sudo pacman -S firefox plasma sddm konsole dolphin
+sudo systemctl enable sddm
+sudo systemctl status sddm
+sudo pacman -S networkmanager
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+sudo pacman -S linux-firmware
+sudo pacman -S wl-clipboard
+sudo pacman -S endeavouros-branding eos-settings-plasma eos-breeze-sddm
