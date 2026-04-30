17390d0a7dcfea08556f5302ead317739aad1982
IlyaGredasov <iljagredasov@yandex.ru>
2026-04-30 21:56:41 +0300

    feat: add PowerShell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+Import-Module PSReadLine
+Set-Alias -Name np -Value notepad3.exe
+Set-Alias -Name mc -Value micro.exe
+function rm-rf {
+    param([Parameter(Mandatory=$true)][string]$Path)
+    Remove-Item -Path $Path -Recurse -Force
+}
+function grep-hist {
+    param($pattern)
+    get-content (Get-PSReadlineOption).HistorySavePath | Select-String -Pattern $pattern
+}
