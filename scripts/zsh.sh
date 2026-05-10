f1f27652b8fa60fd2137e0041bd06ee03849d7f6
IlyaGredasov <iljagredasov@yandex.ru>
2026-05-10 08:57:54 +0300

    refactor: change repo structure

--- scripts/zsh.sh
+++
@@ -1,3 +0,0 @@
-sudo useradd -m -s /bin/zsh -G sudo ilya
-sudo passwd ilya
-chsh -s /bin/zsh
--- windows/configs/Microsoft.PowerShell_profile.ps1
+++ windows/configs/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,7 @@
 Import-Module PSReadLine
 Set-Alias -Name np -Value notepad3.exe
 Set-Alias -Name mc -Value micro.exe
-function ps-history { & notepad3.exe (get-PSReadlineOption).HistorySavePath $args }
-function ffmpeg { & ffmpeg.exe -hide_banner $args }
-function ffprobe { & ffprobe.exe -hide_banner $args }
-function ffplay { & ffplay.exe -hide_banner $args }
+function ps-history { & notepad3.exe (get-PSReadlineOption).HistorySavePath }
 function rm-rf {
     param([Parameter(Mandatory=$true)][string]$Path)
     Remove-Item -Path $Path -Recurse -Force
