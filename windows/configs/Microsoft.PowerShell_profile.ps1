80b4a11b8cc921d0ecd3bdd5c31b3387165e6245
IlyaGredasov <iljagredasov@yandex.ru>
2026-05-29 17:54:05 +0300

    fix: replace notepad3 by notepad++

--- windows/configs/Microsoft.PowerShell_profile.ps1
+++ windows/configs/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 Import-Module PSReadLine
 Set-Alias -Name np -Value notepad++.exe
 Set-Alias -Name mcr -Value micro.exe
-function ps-history { & notepad3.exe (get-PSReadlineOption).HistorySavePath }
+function ps-history { & notepad++.exe (get-PSReadlineOption).HistorySavePath }
 function rm-rf {
     param([Parameter(Mandatory=$true)][string]$Path)
     Remove-Item -Path $Path -Recurse -Force
