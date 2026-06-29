8f83659386d6076c6ae677e0b4ea38617091d3f2
IlyaGredasov <iljagredasov@yandex.ru>
2026-06-29 13:23:07 +0300

    fix: solve rename_dataset wrong behaviour

--- common/scripts/rename_dataset.py
+++ common/scripts/rename_dataset.py
@@ -1,15 +1,30 @@
 import argparse
 import os
+import uuid
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path")
-parser.add_argument("--prefix", required=True)
+parser.add_argument("--prefix", default="")
 args = parser.parse_args()
 
 dir_path = os.path.abspath(args.path)
+base_prefix = f"{args.prefix}_" if args.prefix else ""
 
-for i, name in enumerate(sorted(os.listdir(dir_path)), start=1):
+files = [
+    name
+    for name in sorted(os.listdir(dir_path))
+    if os.path.isfile(os.path.join(dir_path, name))
+]
+
+temp_files = []
+
+for name in files:
     src = os.path.join(dir_path, name)
-    if os.path.isfile(src):
-        dst = os.path.join(dir_path, f"{args.prefix}_{i:03d}.png")
-        os.rename(src, dst)
+    tmp_name = f"{uuid.uuid4().hex}{os.path.splitext(name)[1]}"
+    tmp = os.path.join(dir_path, tmp_name)
+    os.rename(src, tmp)
+    temp_files.append(tmp)
+
+for i, tmp in enumerate(temp_files, start=1):
+    dst = os.path.join(dir_path, f"{base_prefix}{i:03d}.png")
+    os.rename(tmp, dst)
