ff1dca3ec40f27d0488006bd0c558854f64d5675
IlyaGredasov <iljagredasov@yandex.ru>
2026-07-07 21:59:56 +0300

    feat: update links scripts

---
+++ linux/scripts/update_links.sh
@@ -0,0 +1,12 @@
+ln -s ~/Programming/Environment/common/configs/.clang-format ~/Programming/C++/.clang-format
+ln -s ~/Programming/Environment/common/configs/.clang-tidy ~/Programming/C++/.clang-tidy
+ln -s ~/Programming/Environment/common/configs/.clangd ~/Programming/C++/.clangd
+ln -s ~/Programming/Environment/common/configs/.gitconfig ~/.gitconfig
+ln -s ~/Programming/Environment/common/configs/micro_settings.json ~/.config/micro/settings.json
+ln -s ~/Programming/Environment/linux/configs/micro_bindings.json ~/.config/micro/bindings.json
+ln -s ~/Programming/Environment/linux/configs/vs_code_settings.json ~/.config/Code/$(whoami)/settings.json
+ln -s ~/Programming/Environment/linux/configs/.orange.dircolors ~/.orange.dircolors
+ln -s ~/Programming/Environment/linux/configs/.violet.dircolors ~/.violet.dircolors
+ln -s ~/Programming/Environment/linux/configs/.zshrc ~/.zshrc
+sudo ln -s ~/Programming/Environment/linux/configs/orange.profile /usr/share/konsole/orange.profile
+sudo ln -s ~/Programming/Environment/linux/configs/violet.profile /usr/share/konsole/violet.profile
