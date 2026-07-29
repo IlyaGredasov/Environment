0284ad3ca851a68f4298573fe505d80880e368ea
IlyaGredasov <iljagredasov@yandex.ru>
2026-07-29 20:46:09 +0300

    feat: update PSReadLine config

--- windows/configs/Microsoft.PowerShell_profile.ps1
+++ windows/configs/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,16 @@
 Import-Module PSReadLine
+
+Set-PSReadLineOption `
+    -MaximumHistoryCount 400000 `
+    -HistorySaveStyle SaveIncrementally `
+    -HistoryNoDuplicates `
+    -HistorySearchCursorMovesToEnd `
+    -PredictionSource HistoryAndPlugin `
+    -PredictionViewStyle InlineView `
+    -ShowToolTips
+
+Set-PSReadLineKeyHandler -Chord Ctrl+Spacebar -Function MenuComplete
+
 Set-Alias -Name np -Value notepad++.exe
 Set-Alias -Name mcr -Value micro.exe
 function ps-history { & notepad++.exe (get-PSReadlineOption).HistorySavePath }
